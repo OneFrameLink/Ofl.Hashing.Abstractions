6a2cd55088323995c24821b890a40f55ace7bfec
casperOne <casperOne@caspershouse.com>
2017-05-21 17:19:04 -0400

    Initial code push.

---
+++ src/Ofl.Hashing.Abstractions/IHashAlgorithm.cs
@@ -0,0 +1,11 @@
+﻿namespace Ofl.Hashing
+{
+    public interface IHashAlgorithm
+    {
+        int HashSize { get; }
+
+        void TransformBlock(byte[] bytes, int offset, int count);
+
+        byte[] Hash { get; }
+    }
+}
